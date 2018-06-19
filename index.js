995069a2a4ca7a8898f60634e95c66768471a262
Yetunde Okediji <yetunde.okediji@gmail.com>
2018-06-19 22:07:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var musicians =["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"] //array of musicians
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]//array of instruments
+for theBeatlesPlay function() {
+  let i =
+}
